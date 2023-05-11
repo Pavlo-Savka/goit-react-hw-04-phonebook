97a2e2e2da307baa7e08f132968755f37833661e
Pavlo-Savka <savka.pavlo@gmail.com>
2023-05-12 00:48:26 +0300

    done

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,22 +3,27 @@ import PropTypes from 'prop-types';
 import css from './Form.module.css'
 
 function Form(props) {
-  const [phoneBookForm, setPhoneBookForm] = useState({ name: '', number:''});
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
  
- const handleInputChange = event => {
-   setPhoneBookForm({ [event.currentTarget.name]: event.currentTarget.value });
+  const handleInputChange = event => {
+    const { name, value } = event.currentTarget;
+    if (name === 'name') {
+      setName(value);
+    } else if (name === 'number') {
+      setNumber(value);
+    }
   };
  const handleSubmit = event => {
-    event.preventDefault();
-   props.onSubmit(phoneBookForm);
-    reset();
+   event.preventDefault();
+   props.onSubmit({name, number})
+   reset();
   };
- const reset = () => 
-   setPhoneBookForm({
-    name: '',
-    number: '',
-  })
-  
+  const reset = () => {
+    setName('',)
+    setNumber('',)
+  }
+ 
     return (
       <form
         action="addContact"
@@ -35,7 +40,7 @@ function Form(props) {
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
-            value={phoneBookForm.name}
+            value={name}
             onChange={handleInputChange}
           />
         </label>
@@ -48,7 +53,7 @@ function Form(props) {
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
-            value={phoneBookForm.number}
+            value={number}
             onChange={handleInputChange}
           />
         </label>
